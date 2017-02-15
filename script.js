e992d7182d25306b3476648cf94b4b3019e0cb33
Yefry Rodriguez <yefry.r.student@lyonscommunityschool.org>
2017-02-15 15:17:30 +0000

    built house

---
+++ script.js
@@ -0,0 +1,14 @@
+ TinyTurtle.apply(window); 
+
+ right(180);
+ forward(50);
+ right(90);
+ forward(80);
+ right(90);
+ forward(50); 
+ right(90);
+ forward(80);
+ left(120);
+ forward(80);
+ left(120);
+ forward(80);
